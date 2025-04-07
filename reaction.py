cf633a095630f450f5bc6112cf9027259a477318
xzy <715660750@qq.com>
2025-04-07 15:11:06 +0800

    V4

--- reaction.py
+++ reaction.py
@@ -6,6 +6,9 @@ led=LED(4)
 right_button = Button(15)
 left_button = Button(14)
 
+left_name = input('left player name is')
+right_name = input('right player name is')
+
 
 led.on()
 sleep(uniform(5,10))
@@ -16,3 +19,10 @@ def pressed(button):
 
 right_button.when_pressed = pressed
 left_button.when_pressed = pressed
+
+if button.pin.number == 14:
+  print(left_name + 'won the game')
+else:
+  print(right_name + 'won the game')
+
+exit()
