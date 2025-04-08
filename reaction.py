347972dcbc9302c91a87bb99ebe0147f931b279d
XuZhenyu-NUIST <715660750@qq.com>
2025-04-08 20:56:18 +0800

    yy

--- reaction.py
+++ reaction.py
@@ -3,26 +3,26 @@ from time import sleep
 from random import uniform
 
 led=LED(4)
+i=1
 right_button = Button(15)
 left_button = Button(14)
+while i>0:
+   left_name = input('left player name is ')
+   right_name = input('right player name is ')
+   i-=1
 
-left_name = input('left player name is')
-right_name = input('right player name is')
 
+   led.on()
+   sleep(uniform(5,10))
+   led.off()
 
-led.on()
-sleep(uniform(5,10))
-led.off()
+   def pressed(button):
+     if button.pin.number ==14:
+       print(left_name + ' won the game')
+     else:
+       print(right_name + ' won the game')
+     exit()
+   right_button.when_pressed = pressed
+   left_button.when_pressed = pressed
 
-def pressed(button):
-  print(str(button.pin.number) + 'won the game')
-
-right_button.when_pressed = pressed
-left_button.when_pressed = pressed
-
-if button.pin.number == 14:
-  print(left_name + 'won the game')
-else:
-  print(right_name + 'won the game')
-
-exit()
+   sleep(2)
